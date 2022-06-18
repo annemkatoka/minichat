cb5b633d632d02b60b7fecb06df29d3933b8f8f9
Ana De Souza <ana.desouzag@outlook.com>
2022-06-18 17:36:54 -0400

    Enable cors

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const io = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require("./sockets");
 
@@ -21,6 +22,7 @@ class Server {
 
     middlewares(){
         this.app.use(express.static( path.resolve(__dirname, '../public')));
+        this.app.use(cors);
     }
 
     configurarSockets(){
